2114df1b6f718eb92446a919cdad0e59bd3bf497
vmuguerzac <vmuguerzac@gmail.com>
2026-04-27 22:22:59 -0500

    Agregago S6 funcion sin parametros

---
+++ FundAlgoritmos/s6_funcion_sin_parametros.cs
@@ -0,0 +1,10 @@
+MostrarMenu();
+
+
+void MostrarMenu()
+{
+    Console.WriteLine("###### Bienvenido a mi Sistema ######");
+    Console.WriteLine("Seleccione su Opción: ");
+    Console.WriteLine("1. Calcular");
+    Console.WriteLine("2. Salir");
+}
